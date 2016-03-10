4e73844ba58bdcc8f285c18a2a008a4b20fe04a1
CastellaFactory <castellafactory@gmail.com>
2016-03-11 02:51:21 +0900

    test

--- spec/slack-qiita-daily-bot_spec.cr
+++ spec/slack-qiita-daily-bot_spec.cr
@@ -4,6 +4,7 @@ describe Slack::Qiita::Daily::Bot do
   it "Get JSON" do
     resp = HTTP::Client.get("https://raw.githubusercontent.com/qaleidospace/qaleidospace.github.io/master/_data/daily.json")
     result = Array(Slack::Qiita::Daily::Bot::PostData).from_json(resp.body)
+
     (result.size > 0).should be_true
   end
 end
